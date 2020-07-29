649e356b3c46edd9c72376e328e5f773e5b23777
RahatGithub <rahatahmedchowdhuryrac@gmail.com>
2020-07-29 06:08:03 +0600

    second commit

--- src/App.js
+++ src/App.js
@@ -2,23 +2,42 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const person = [
+  {name: 'Mahmudul Hasan', designation: 'CEO', salary: '$2650'},
+  {name: 'Kamrul Islam', designation: 'Director', salary: '$2000'},
+  {name: 'Muminur Rashid', designation: 'HR head', salary: '$1560'},
+];
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Greet info={person[0]}></Greet>
+        <Greet info={person[1]}></Greet>
+        <Greet info={person[2]}></Greet>
       </header>
     </div>
   );
 }
+
+function Greet(props){
+  
+  const makeUp={
+    color: 'cyan',
+    margin: '5px',
+    border: '2px solid blue',
+    width: '300px',
+  }
+  const {name, designation, salary} = props.info;
+  return (
+    <div style={makeUp}>
+      <h3>Hello {name}</h3>
+      <h5> {designation} </h5>
+      <h5> {salary} </h5>
+    </div>
+  );
+}
+
 
 export default App;
