bff2a7b2c5dcee483fa0ef96903c826ca0c1e9ad
RahatGithub <rahatahmedchowdhuryrac@gmail.com>
2020-07-30 01:23:26 +0600

    Summary of react intro

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -9,16 +9,28 @@ const products = [
   {name: 'Gamdias motherboard', price: 'BDT.6600'},
   {name: 'MaxGreen casing', price: 'BDT.2200'},
 ];
+const employees = [
+  {name: 'Thomas Harry', designation: 'CEO', joined: '22/03/11'},
+  {name: 'David Billy', designation: 'Director', joined: '14/08/15'},
+  {name: 'Paul Kent', designation: 'Manager', joined: '11/02/16'},
+  {name: 'Sagar Desai', designation: 'HR Head', joined: '04/06/13'},
+];
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Count></Count>
+        <Hours></Hours>
+        <ul>
+          {
+            employees.map(emp => <Employees employee={emp}></Employees>)
+          }
+        </ul>
+        <Customers></Customers>
           <ul>
             {
-              products.map(pd=><ShowProducts product={pd}></ShowProducts>)
+              products.map(pd => <ShowProducts product={pd}></ShowProducts>)
             }
           </ul>
       </header>
@@ -26,13 +38,86 @@ function App() {
   );
 }
 
+function Hours(){
+  const btnMakeUp = {
+    backgroundColor: "gold",
+    border: 'none',
+    borderRadius: '5px',
+    padding: '10px',
+    margin: '5px',
+  }
+  const [hours, setHours] = useState(0);
+  const addHandler = () => {setHours(hours+1)};
+  const reduceHandler = () => {
+    if(hours>0){
+      setHours(hours-1);
+    }
+  };
+  const resetHandler = () => {setHours(0)};
+  return(
+    <div>
+      <h4>Hours at work: {hours}</h4>
+      <button style={btnMakeUp} onClick={addHandler}>Add</button>
+      <button style={btnMakeUp} onClick={reduceHandler}>Reduce</button>
+      <button style={btnMakeUp} onClick={resetHandler}>Reset</button>
+    </div>
+  )
+}
+
+function Employees(props){
+  const EmployeesStyle = {
+    color: "rgb(98, 218, 253)",
+    backgroundColor: 'black',
+    border: '2px solid blue',
+    borderRadius: '15px',
+    margin: '10px',
+    padding: '10px',
+    margin: '10px',
+    width: '200px',
+  }
+  const contactStyle = {
+    color: "rgb(98, 218, 253)",
+    backgroundColor: 'blue',
+    border: 'none',
+    borderRadius: '5px',
+    padding: '10px',
+  }
+  const {name, designation, joined} = props.employee;
+  return(
+    <div style={EmployeesStyle}>
+      <h4>{name}</h4>
+      <h5>{designation}</h5>
+      <h6>Joined: {joined}</h6>
+      <button style={contactStyle}>Contact</button>
+    </div>
+  )
+}
+
+function Customers(){
+  const [customers, setCustomers] = useState([]);
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then(response => response.json())
+    .then(data => setCustomers(data))
+  });
+  return(
+    <div>
+      <h3>Customers:</h3>
+      <ul>
+        {
+          customers.map(c => <li>{c.name}</li>)
+        }
+      </ul>
+    </div>
+  )
+}
 function ShowProducts(props){
   const makeUp={
-    color: 'cyan',
+    color: "rgb(98, 218, 253)",
     backgroundColor: 'black',
     margin: '10px',
     padding: '5px',
-    border: '2px solid blue',
+    border: '2px solid gold',
     borderRadius: '10px',
   }
   const buyNowBtn = {
@@ -40,47 +125,17 @@ function ShowProducts(props){
     border: 'none',
     borderRadius: '5px',
     height: '30px',
+    margin: '5px',
+    padding: '5px',
   }
   const {name, price} = props.product;
   return (
     <div style={makeUp}>
       <h4>{name}</h4>
       <p>Price: {price}</p>
+      <button style={buyNowBtn}>Add to cart</button>
       <button style={buyNowBtn}>Buy now</button>
     </div>
-  );
-}
-
-function Count(){
-  const BtnStyle = {
-    color: 'gold',
-    backgroundColor: 'blue',
-    border: 'none',
-    borderRadius: '2px',
-    padding: '5px',
-    margin: '5px',
-  }
-  const [count, setCount] = useState(0);
-  const addHandler = () => {
-    const newCount = count + 1;
-    setCount(newCount);
-  }
-  const reduceHandler = () => {
-    if(count>0){
-      const newCount = count - 1;
-      setCount(newCount);
-    }
-  }
-  const resetHandler = () => {
-    setCount(0);
-  }
-  return(
-    <span>
-      <h1>Count: {count}</h1>
-      <button style={BtnStyle} onClick={addHandler}>Add</button>
-      <button style={BtnStyle} onClick={reduceHandler}>Reduce</button> 
-      <button style={BtnStyle} onClick={resetHandler}>Reset</button>
-    </span>
   );
 }
 
