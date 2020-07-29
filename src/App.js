095b39b88760bc36f92b0d7e1ab09b27726d73ce
RahatGithub <rahatahmedchowdhuryrac@gmail.com>
2020-07-29 12:33:03 +0600

    Dynamic component and event listener

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const person = [
-  {name: 'Mahmudul Hasan', designation: 'CEO', salary: '$2650'},
-  {name: 'Kamrul Islam', designation: 'Director', salary: '$2000'},
-  {name: 'Muminur Rashid', designation: 'HR head', salary: '$1560'},
+const products = [
+  {name: 'DELL 19" monitor', price: 'BDT.13,500'},
+  {name: 'HP 19.5" monitor', price: 'BDT.6,500'},
+  {name: 'A4Tech Keyboard', price: 'BDT.780'},
+  {name: 'Gamdias motherboard', price: 'BDT.6600'},
+  {name: 'MaxGreen casing', price: 'BDT.2200'},
 ];
 
 function App() {
@@ -13,31 +15,73 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Greet info={person[0]}></Greet>
-        <Greet info={person[1]}></Greet>
-        <Greet info={person[2]}></Greet>
+        <Count></Count>
+          <ul>
+            {
+              products.map(pd=><ShowProducts product={pd}></ShowProducts>)
+            }
+          </ul>
       </header>
     </div>
   );
 }
 
-function Greet(props){
-  
+function ShowProducts(props){
   const makeUp={
     color: 'cyan',
-    margin: '5px',
+    backgroundColor: 'black',
+    margin: '10px',
+    padding: '5px',
     border: '2px solid blue',
-    width: '300px',
+    borderRadius: '10px',
   }
-  const {name, designation, salary} = props.info;
+  const buyNowBtn = {
+    backgroundColor: 'gold',
+    border: 'none',
+    borderRadius: '5px',
+    height: '30px',
+  }
+  const {name, price} = props.product;
   return (
     <div style={makeUp}>
-      <h3>Hello {name}</h3>
-      <h5> {designation} </h5>
-      <h5> {salary} </h5>
+      <h4>{name}</h4>
+      <p>Price: {price}</p>
+      <button style={buyNowBtn}>Buy now</button>
     </div>
   );
 }
 
+function Count(){
+  const BtnStyle = {
+    color: 'gold',
+    backgroundColor: 'blue',
+    border: 'none',
+    borderRadius: '2px',
+    padding: '5px',
+    margin: '5px',
+  }
+  const [count, setCount] = useState(0);
+  const addHandler = () => {
+    const newCount = count + 1;
+    setCount(newCount);
+  }
+  const reduceHandler = () => {
+    if(count>0){
+      const newCount = count - 1;
+      setCount(newCount);
+    }
+  }
+  const resetHandler = () => {
+    setCount(0);
+  }
+  return(
+    <span>
+      <h1>Count: {count}</h1>
+      <button style={BtnStyle} onClick={addHandler}>Add</button>
+      <button style={BtnStyle} onClick={reduceHandler}>Reduce</button> 
+      <button style={BtnStyle} onClick={resetHandler}>Reset</button>
+    </span>
+  );
+}
 
 export default App;
